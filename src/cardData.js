a7a37b106d18b250e82c3649f840472f803f07d7
Phoebe Quincy <phoebequincy@gmail.com>
2019-06-03 18:14:06 -0600

    Added all social card content for basic backbone.

---
+++ src/cardData.js
@@ -0,0 +1,13 @@
+const cardData = {
+  headline: 'Social Cards are Important!',
+
+  name: 'Your Name',
+
+  message: "Social cards are an important part of your visual branding, and every website should have them. Even if you aren’t using social media actively yourself (…because you live under a rock?) you never know who might share your content, and it’s important that your brand looks great every time.",
+
+  url: 'https://yourwebsite.com',
+
+  urlDisplayName: 'yourname.com',
+}
+
+export default cardData;
---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import card from '../cardData';
+import Card from './Card';
+import headshot from '../self.jpg';
+import sand from '../sand.jpg';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Card
+          logoURL={sand}
+          headline={card.headline}
+          name={card.name}
+          creditImageURL={headshot}
+          message={card.message}
+          url={card.url}
+          urlDisplayName={card.urlDisplayName}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
